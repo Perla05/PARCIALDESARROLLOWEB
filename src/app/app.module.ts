c6867bc2bff0519cbfbbfaf9cb647c41dca79856
unknown <perla.bless@gmail.com>
2021-08-14 04:16:16 -0600

    Iniciando con la cabecera del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {CabeceraModule} from "./cabecera/cabecera.module";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CabeceraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
