c0dc39367176d0c4672600ebaa8d7c1bcbc110a6
unknown <perla.bless@gmail.com>
2021-08-14 20:33:04 -0600

    agregamos todos los archivos del componente del sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {CabeceraModule} from "./cabecera/cabecera.module";
+import {SidebarModule} from "./sidebar/sidebar.module";
+
 
 
 @NgModule({
@@ -11,7 +13,8 @@ import {CabeceraModule} from "./cabecera/cabecera.module";
   ],
   imports: [
     BrowserModule,
-    CabeceraModule
+    CabeceraModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
